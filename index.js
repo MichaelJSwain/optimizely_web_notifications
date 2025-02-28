3c0ad8def6b8b8bd3b49f3ee53e5693b3bdf994c
Michael Swain <michaelswain@tommy.com>
2025-02-28 15:53:17 +0100

    option to use change history endpoint instead

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const { default: axios } = require("axios");
 require('dotenv').config();
-const { OPTLY_TOKEN, TH_PROD_PROJECT_ID, TH_QA_AUDIENCE_ID, MONGODB_URI } = process.env;
+const {OPTLY_TOKEN, TH_QA_AUDIENCE_ID} = process.env;
 
-const db = {
-    runningExperimentIDs: [5092054213066752, 5013064228012032, 4734509292191744, 4697687396712448],
-    pausedExperimentIDs: [],
-    archivedExperimentIDs: []
-}
-
-const fetchRunningTests = async () => {
+const optimizelyRequest = async (endpoint, method, body = false) => {
     const options = {
         method: 'GET',
         headers: {
@@ -17,76 +10,100 @@ const fetchRunningTests = async () => {
         }
       };
       
-      const tests = await fetch(`https://api.optimizely.com/v2/search?per_page=50&page=1&query=-&project_id=${TH_PROD_PROJECT_ID}&type=experiment&expand=&archived=false&fullsearch=false&status=running`, options)
+      const result = await fetch(endpoint, options)
         .then(res => res.json())
-        .then(res => {
-            const exps = []
-            res.forEach(exp => {
-                const isTracked = db.runningExperimentIDs.some(id => id == exp.id);
-                if (!isTracked) {
-                    exps.push(exp);
-                }
-            });
-            return exps;
-        })
+        .then(res => res)
         .catch(err => console.error(err));
-        return tests
+      return result;
 }
 
-const persistExp = (exp_id) => {
-    db.runningExperimentIDs.push(exp_id);
-}
+const getTimestamps = () => {
+    const endTimestamp = Date.now();
+    const startTimestamp = Date.now() - 9600000;
 
-const checkIsRunningForUsers = async (experiments) => {
-    const runningTests = [];
-    for (exp of experiments) {
-        if (exp.id) {
-            const options = {
-                method: 'GET',
-                headers: {
-                  accept: 'application/json',
-                  authorization: OPTLY_TOKEN
-                }
-              };
-              
-              const isRunning = await fetch(`https://api.optimizely.com/v2/experiments/${exp.id}`, options)
-                .then(res => res.json())
-                .then(res => {
-                    if (!res.audience_conditions.includes(TH_QA_AUDIENCE_ID) || 
-                        (!res.audience_conditions.includes("and") && 
-                        res.audience_conditions.includes(TH_QA_AUDIENCE_ID))) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                })
-                .catch(err => console.error(err));
+    const endTimestampISO = new Date(endTimestamp).toISOString();
+    const startTimestampISO = new Date(startTimestamp).toISOString();
 
-                if (isRunning) {
-                    runningTests.push(exp);
-                    runningTests.push(isRunning);
-                    persistExp(exp.id);
-                }
-                
-        }
-
+    return {
+        start_time: startTimestampISO,
+        end_time: endTimestampISO
     }
-    console.log(runningTests);
-    return runningTests;
 }
 
-const checkOptimizely = async () => {
-    const runningTests = await fetchRunningTests(); 
-    const runningForUsers = await checkIsRunningForUsers(runningTests);  
-    // console.log(runningForUsers);
+const checkForUpdatedExperimentStatus = (changeHistory) => {
+    const experimentIDs = {};
+
+    // changeHistory.forEach(item => {
+    //     console.log(item);
+    // })
+
+    for (item of changeHistory) {
+        if (item.changes) {
+            for (change of item.changes) {
+                if (change.property && change.property === "status" && !experimentIDs[item.entity.id]) {
+                    
+                    experimentIDs[item.entity.id] = {
+                        exp_id: item.entity.id,
+                        exp_name: item.entity.name,
+                        exp_status: change.after
+                    }
+                }
+            }
+        }
+    }
+    return experimentIDs;
 }
 
+const checkChangeHistory = async () => {
+    const {start_time, end_time} = getTimestamps();
+    const changeHistory = await optimizelyRequest(`https://api.optimizely.com/v2/changes?project_id=26081140005&start_time=${start_time}&end_time=${end_time}&per_page=25&page=1`)
+    return changeHistory; 
+}
 
+const checkTargeting = async (updatedExperiments) => {
+    const launchedExperiments = [];
+    const keys = Object.keys(updatedExperiments);
 
+    for (const key of keys) {
+        console.log(key);
+        const foundExperiment = await optimizelyRequest(`https://api.optimizely.com/v2/experiments/${updatedExperiments[key].exp_id}`);
+        let isRunningInQAMode;
+        if (foundExperiment) {
+            isRunningInQAMode = false;
+            if (!foundExperiment.audience_conditions.includes(TH_QA_AUDIENCE_ID) || 
+                (!foundExperiment.audience_conditions.includes("and") && 
+                foundExperiment.audience_conditions.includes(TH_QA_AUDIENCE_ID))) {
+                
+                // check page targeting too
+                if (foundExperiment.page_ids.length) {
+                    const page_id = foundExperiment.page_ids[0];
+                    const foundPage = await optimizelyRequest(`https://api.optimizely.com/v2/pages/${page_id}`);
+                    if (foundPage && foundPage.conditions && foundPage.conditions.includes("devtestp")) {
+                        isRunningInQAMode = true;
+                    } else {
+                        isRunningInQAMode = false;
+                    }
+                }
+            } else {
+                isRunningInQAMode = true;
+            }
 
-// const hourInMilliseconds = 3600000;
-const interval = 10000;
-// setInterval(() => {
-//     console.log("checking Optly project");
-    // checkOptimizely()
-// }, interval);
+            if (!isRunningInQAMode) {
+                launchedExperiments.push(updatedExperiments[key]);
+            }
+        }
+    }
+    return launchedExperiments;
+}
+
+const main = async () => {
+    const changeHistory = await checkChangeHistory();
+    if (changeHistory.length) {
+        const updatedExperiments = checkForUpdatedExperimentStatus(changeHistory);
+        if (updatedExperiments) {
+            const result = await checkTargeting(updatedExperiments);
+            console.log(result);
+        }
+    }
+}
+main();
