c8a5a35f5013d5e9b0d3097f12f9b88d2cb33db9
Michael Swain <michaelswain@tommy.com>
2025-03-10 09:11:14 +0100

    add return statement in get route

--- index.js
+++ index.js
@@ -316,6 +316,7 @@ app.get("/pvh/optimizelyWeb/notifications", (req, res) => {
     console.log("req object = ", req);
     console.log("req body = ", req.body);
     checkWebProjects();
+    return;
 });
 
 app.listen(PORT, (req, res) => {
